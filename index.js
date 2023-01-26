a601957f011ae7135b875e53082575b8f9e2c043
ve1kka <veikka.salmela@gmail.com>
2023-01-26 22:53:29 +0200

    3.2

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ app.get('/api/persons', (req, res) => {
   res.json(notes)
 })
 
+app.get('/info', (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${notes.length} people</p>` +
+    `<p>${new Date()}</p>`
+    )
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
