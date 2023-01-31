9a4176a25cddffaa384a360a67bb4c1e14ac0149
ve1kka <veikka.salmela@gmail.com>
2023-01-31 15:12:12 +0200

    3.12

---
+++ mongo.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  )
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.owh7r17.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  id: Number,
+  number: String,
+})
+
+const generateId = () => { 
+  return Math.floor(Math.random() * 1000000)
+}
+
+const Person = mongoose.model("Person", personSchema)
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    console.log("Phonebook: ")
+    result.forEach((person) => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  const person = new Person({
+    id: generateId(),
+    name: name,
+    number: number,
+  })
+
+  person.save().then((_result) => {
+    console.log(`Added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
